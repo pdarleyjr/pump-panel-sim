f0911bd7c574ba183bcb0823a990b67fa312411f
pdarleyjr <pdarleyjr@gmail.com>
2025-10-17 11:28:28 -0400

    chore: trigger Pages rebuild to apply updated CSP headers (remove sha256 hash)

--- pump-panel-sim/src/main.tsx
+++ pump-panel-sim/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Trigger Pages rebuild to pick up updated _headers CSP configuration
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
